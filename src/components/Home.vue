7b14779b487e3ae78127a5c08e722e02ec9e00a0
baihua@sndskj.com <baihua2018>
2018-08-17 08:47:37 +0800

    支持 stylus 样式

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
     <Header></Header>
+    <div class="tab">
+      <div class="tab-item">商品</div>
+      <div class="tab-item">评论</div>
+      <div class="tab-item">商家</div>
+    </div>
     this is a home
   </div>
 </template>
@@ -10,10 +15,17 @@ import Header from './Header'
 
 export default {
   name: 'Home',
-  comments: {Header}
+  components: {Header}
 }
 </script>
 
-<style scoped>
-
+<style lang="stylus" rel="stylesheet/stylus" scoped>
+.tab
+  display: flex
+  width: 100%
+  height: 40px
+  line-height: 40px
+  .tab-item
+    flex: 1
+    text-align: center
 </style>
